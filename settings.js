ea95d219f20626f9b1353d8dadfcab14ee838db0
Allan Dawid <allan.queiroz.dev@gmail.com>
2023-09-23 17:15:05 -0300

    adicionada seleção de tema

--- settings.js
+++ settings.js
@@ -1,14 +1,23 @@
 const toggles = document.querySelectorAll(".settings [type='checkbox']");
+const radios = document.querySelectorAll(".settings [type='radio']");
 const doc = document.documentElement;
 
 function updateSiteUi({ name, value }) {
-    return doc.dataset[name] = value;
+  return (doc.dataset[name] = value);
 }
 
 toggles.forEach((toggle) => {
-  toggle.addEventListener("change", (e) => {
-    const { name, checked } = e.target;
+  toggle.addEventListener("change", (event) => {
+    const { name, checked } = event.target;
     updateSiteUi({ name, value: checked });
     localStorage.setItem(name, checked);
+  });
+});
+
+radios.forEach((radio) => {
+  radio.addEventListener("change", (event) => {
+    const { name, id } = event.target;
+    updateSiteUi({ name, value: id });
+    localStorage.setItem(name, id);
   });
 });
